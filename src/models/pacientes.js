e38636f4fd8c0b813a0370aaf132da7a800a5dda
Nicolas Cortez D Elia <nicocdelia@gmail.com>
2022-02-27 15:31:29 -0300

    realizo el modelo de los objetos turno y paciente

--- src/models/pacientes.js
+++ src/models/pacientes.js
@@ -1,2 +1,54 @@
 //aquí se modela el dato que se guarda en la Base de Datos
 
+import mongoose, {Schema} from "mongoose";
+
+const pacienteSchema = new Schema({
+    nombre:{
+        type: String,
+        maxlength: 23,
+        minlength: 0,
+        required: true
+
+    },
+    apellido:{
+        type: String,
+        maxlength: 23,
+        minlength: 0,
+        required: true
+
+    },
+    eMail:{
+        type: String,
+        required: true
+
+    },
+    telefono:{
+        type: Number,
+        required: true
+
+
+    } ,
+    nombreMascota: {
+        type: String,
+        maxlength: 23,
+        minlength: 0,
+        required: true
+
+    },
+    especie: {
+        type: String,
+        maxlength: 23,
+        minlength: 0,
+        required: true
+    },
+    raza: {
+        type: String,
+        maxlength: 23,
+        minlength: 0,
+        required: true
+
+    }
+
+});
+
+const PacienteModelo = mongoose.model("paciente", pacienteSchema)
--- src/models/turnos.js
+++ src/models/turnos.js
@@ -1,1 +1,33 @@
 //aquí se modela el dato que se guarda en la Base de Datos
+
+import mongoose, {Schema} from "mongoose";
+
+const turnoSchema = new Schema({
+    TurnoPetName:{
+        type: String,
+        maxlength: 23,
+        minlength: 0,
+        required: true
+
+
+    },
+    TurnoDoctor:{
+        type: String,
+        maxlength: 200,
+        required: true
+
+    },
+    TurnoDetalle:{
+        type: String,
+        required: true
+
+    },
+    TurnoFecha:{
+        unique: true,
+        required: true
+
+    }
+   
+});
+
+const TurnoModelo = mongoose.model("turno", turnoSchema);
